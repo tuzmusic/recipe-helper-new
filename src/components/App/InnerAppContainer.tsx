3be2e39ff82f20f0be01e29fc802e77048287206
Jonathan Tuzman <jtuzman@sunriselabs.com>
2019-11-10 18:25:20 -0500

    rename CookingSession to MainCookingScreen

--- src/components/App/InnerAppContainer.tsx
+++ src/components/App/InnerAppContainer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from 'redux-bundler-react'
 import ImportRecipeFormContainer from "../ImportRecipeForm/ImportRecipeFormContainer";
-import CookingSessionContainer from "../CookingSession/CookingSessionContainer";
+import CookingSessionContainer from "../MainCookingScreen/MainCookingScreen";
 
 const InnerAppContainer: React.FC<any> = ({ currentRecipe }) => {
   return (currentRecipe
---
+++ src/components/MainCookingScreen/MainCookingScreen.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import CookingSession from "../CookingSession/CookingSession";
+import RecipeSummaryContainer from "../RecipeSummary/RecipeSummaryContainer";
+
+const MainCookingScreen: React.FC<{}> = () => {
+  return (<Container>
+    <Row>
+      <Col>
+        <RecipeSummaryContainer/>
+      </Col>
+      <Col>
+        <CookingSession/>
+      </Col>
+    </Row>
+  </Container>)
+};
+
+export default MainCookingScreen;
+

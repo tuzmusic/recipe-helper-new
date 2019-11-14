0cdf56314b860a4a125e18c8ceb5ee994e5a2fc2
Jonathan Tuzman <jtuzman@sunriselabs.com>
2019-11-13 22:23:30 -0500

    Revert "fix step validity and step numbering" again?!?!
    This reverts commit 672e646c

--- src/redux/bundles/currentRecipe.ts
+++ src/redux/bundles/currentRecipe.ts
@@ -101,14 +101,6 @@ let selectCurrentStep: BundleSelector<Instruction> = createSelector(
 );
 bundle.selectCurrentStep = selectCurrentStep;
 
-let selectCurrentStepNumber: BundleSelector<number> = createSelector(
-  bundle.selectCurrentRecipe,
-  bundle.selectCurrentStep,
-  (recipe: Recipe, step: Instruction) =>
-    recipe.instructions.indexOf(step) + 1
-);
-bundle.selectCurrentStepNumber = selectCurrentStepNumber;
-
 //endregion
 
 //region ACTIONS

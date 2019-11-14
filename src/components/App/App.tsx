7d1a4a81be8a9cfbc713bb830cd73723b93dd1f6
Jonathan Tuzman <jtuzman@sunriselabs.com>
2019-11-13 19:56:39 -0500

    turn off axios mocking

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import MockAdapter from "axios-mock-adapter";
-import axios from 'axios'
-import { recipeRequest } from "../../utils/requests";
 import { fudgyBrowniesResponse } from "../../api-data/spoonacular-api/fudgy-brownies";
 import createStore from '../../redux/bundles'
 import { Provider } from 'redux-bundler-react'
@@ -9,8 +6,8 @@ import InnerAppContainer from "./InnerAppContainer";
 import Recipe from "../../models/Recipe";
 import { AppState } from "../../redux/bundles/currentRecipe";
 
-const mock = new MockAdapter(axios);
-mock.onGet(recipeRequest().url).reply(200, fudgyBrowniesResponse);
+// const mock = new MockAdapter(axios);
+// mock.onGet(recipeRequest().url).reply(200, fudgyBrowniesResponse);
 
 // const initialState = {}
 const startingRecipe = Recipe.fromSpoonacularApi(fudgyBrowniesResponse);
